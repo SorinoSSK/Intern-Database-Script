5f9ba84c47da1a0ce4b94dd258f59b95bd63fcef
SorinoSSK <sweeapplemage@gmail.com>
2023-04-20 18:40:58 +0800

    .

--- Scripting.py
+++ Scripting.py
@@ -13,7 +13,7 @@ with open('features.txt') as txtFile:
 bltStr = "CREATE TABLE \"dataLog\" (\"uptime\" INTEGER DEFAULT 0, \"timeStamp\" INTEGER DEFAULT 0, \"sentToServer\" INTEGER DEFAULT 0, \"markToSent\" INTEGER DEFAULT 0 "
 bltStr2 = "INSERT INTO dataLog(uptime,timeStamp"
 for i in range(len(varList)):
-    bltStr += "," + varList[i] + " REAL DEAFULT 0"
+    bltStr += "," + varList[i] + " REAL DEAFAULT 0"
     bltStr2 += "," + varList[i]
 bltStr += ");"
 bltStr2 += ") VALUES(?"
