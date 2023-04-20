ccee7e76fd8fc6ec6b9eb0e8a37cf7337a02c025
SorinoSSK <sweeapplemage@gmail.com>
2023-04-21 07:17:35 +0800

    .

---
+++ README.md
@@ -0,0 +1,17 @@
+## If you do not understand the use of this program, then this scripting is not for you :D.
+
+## Software Requirements 
+```Install any python version```
+
+## How to use?
+1. Copy defined variable from your entity table and paste it in a file names "features.txt"
+&nbsp - You do not need to clean up your inputs in this text file.
+2. Run the script
+```
+python Scripting.py
+```
+3. 4 text file will be generated, copy each text to your code. (Text files should have been formatted properly).
+&nbsp - CreateTable.txt (use this to recreate your table)
+&nbsp - InsertBinder.txt (use this to replace your code block in "sql_queries.cpp")
+&nbsp - InsertTable.txt (use this to replace your SQL string in "sql_queries.cpp")
+&nbsp - SQLFetch.txt (use this to replace your code block in "sql_queries.cpp")
--- Scripting.py
+++ Scripting.py
@@ -1,18 +1,28 @@
 
+# Clean up variable's name
 varList = []
+
 with open('features.txt') as txtFile:
-    lines = txtFile.readlines();
+    lines = txtFile.readlines();                    # Read text file into list
+    print("\u001b[4mParameters (For Debugging)\033[0m")
     for i in range(len(lines)):
-        tempVal = lines[i].strip()
-        tempVal = tempVal.replace(";","").split()
-        if len(tempVal) > 1:
-            tempVal = tempVal[1]
-            varList.append(tempVal)
+        tempVal = lines[i].strip()                  # Remove all spaces
+        tempVal = tempVal.replace(";","").split()   # Remove all semi-colon
+        if len(tempVal) > 1:                        # Filter empty list
+            if  tempVal[0][0] != "/":               # Filter comment lines
+                tempVal = tempVal[1]
+                print(i, ")" , tempVal)
+                varList.append(tempVal)
 
 txtFile.close
-
+print("--------------------------")
+print("\033[91m(?) uptime, timeStamp, sentToServer, markToSent will automatically be added to the start of each statement.\033[0m")
+print("\033[91m(?) sentToServer, markToSent will not be retrieved from fetch statement.\033[0m")
+# Build create table and insert statement
 bltStr = "CREATE TABLE \"dataLog\" (\"uptime\" INTEGER DEFAULT 0,\"timeStamp\" INTEGER DEFAULT 0,\"sentToServer\" INTEGER DEFAULT 0,\"markToSent\" INTEGER DEFAULT 0 "
 bltStr2 = "sql = \"INSERT INTO dataLog(uptime,timeStamp,sentToServer,markToSent"
+print("--------------------------")
+print("Building (Create Table) and (Insert) statement")
 for i in range(len(varList)):
     bltStr += "," + varList[i] + " REAL DEFAULT 0"
     bltStr2 += "," + varList[i]
@@ -24,11 +34,16 @@ bltStr2 += ");\";"
 txtFileW = open("CreateTable.txt", "w+")
 txtFileW.write(bltStr)
 txtFileW.close
+print("-> Create Table statement completed!")
 
 txtFileW = open("InsertTable.txt", "w+")
 txtFileW.write(bltStr2)
 txtFileW.close
+print("-> Insert statement completed!")
 
+# Build sqlite3 binders (binds to "?" in values)
+print("--------------------------")
+print("Building C++ Sqlite3 Binders (Bind values to \"?\")")
 txtFileW = open("InsertBinder.txt", "w+")
 txtFileW.write("    rc = sqlite3_bind_double(stmt, loc_count, current_sensor_data->uptime);\n")
 txtFileW.write("    if (rc != SQLITE_OK)\n")
@@ -77,7 +92,11 @@ for i in range(len(varList)):
     txtFileW.write("    loc_count++;\n")
     txtFileW.write("\n")
 txtFileW.close
+print("-> Binders code block completed!")
 
+# Build sqlite3 fetcher that reads from sqlite3
+print("--------------------------")
+print("Building C++ Sqlite3 fetcher (fetch data after reading from Sqlite3)")
 txtFileW = open("SQLFetch.txt", "w+")
 txtFileW.write("        if(strcmp(azColName[i],\"uptime\") == 0)\n")
 txtFileW.write("            fetched_sensor_data->uptime = atol(argv[i]);\n")
@@ -96,4 +115,6 @@ for i in range(len(varList)):
         txtFileW.write("        else if(strcmp(azColName[i],\"" + varList[i] +"\") == 0)\n")
         txtFileW.write("            fetched_sensor_data->battery." + varList[i] + " = atof(argv[i]);\n")
 txtFileW.close
-
+print("-> Fetcher code block completed!")
+print("--------------------------")
+print("\033[4mRefer to \"README.md\" for more instructions.\033[0m")
