36ca690f72f20a6091e94441b4146b6bf8a00a09
SorinoSSK <sweeapplemage@hotmail.com>
2023-04-20 18:56:30 +0800

    .

--- Scripting.py
+++ Scripting.py
@@ -10,16 +10,16 @@ with open('features.txt') as txtFile:
             tempVal = tempVal[1]
             varList.append(tempVal)
 
-bltStr = "CREATE TABLE \"dataLog\" (\"uptime\" INTEGER DEFAULT 0, \"timeStamp\" INTEGER DEFAULT 0, \"sentToServer\" INTEGER DEFAULT 0, \"markToSent\" INTEGER DEFAULT 0 "
-bltStr2 = "INSERT INTO dataLog(uptime,timeStamp"
+bltStr = "CREATE TABLE \"dataLog\" (\"uptime\" INTEGER DEFAULT 0,\"timeStamp\" INTEGER DEFAULT 0,\"sentToServer\" INTEGER DEFAULT 0,\"markToSent\" INTEGER DEFAULT 0 "
+bltStr2 = "sql = \"INSERT INTO dataLog(uptime,timeStamp,sentToServer,markToSent"
 for i in range(len(varList)):
-    bltStr += "," + varList[i] + " REAL DEAFAULT 0"
+    bltStr += "," + varList[i] + " REAL DEFAULT 0"
     bltStr2 += "," + varList[i]
 bltStr += ");"
 bltStr2 += ") VALUES(?"
-for i in range(len(varList)+1):
+for i in range(len(varList)+3):
     bltStr2 += ",?"
-bltStr2 += ")"
+bltStr2 += ");\";"
 txtFileW = open("CreateTable.txt", "w+")
 txtFileW.write(bltStr)
 
