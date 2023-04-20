6a151d515fbfcbc86612483e21dcf2ee10b3494b
SorinoSSK <sweeapplemage@gmail.com>
2023-04-20 18:28:39 +0800

    Simple script

---
+++ Scripting.py
@@ -0,0 +1,28 @@
+
+varList = []
+with open('features.txt') as txtFile:
+    lines = txtFile.readlines();
+    for i in range(len(lines)):
+        tempVal = lines[i].strip()
+        tempVal = tempVal.replace(";","").split(" ")
+        print(tempVal)
+        if len(tempVal) > 1:
+            tempVal = tempVal[1]
+            varList.append(tempVal)
+
+bltStr = "CREATE TABLE \"dataLog\" (\"uptime\" INTEGER DEFAULT 0, \"timeStamp\" INTEGER DEFAULT 0"
+bltStr2 = "INSERT INTO dataLog(uptime,timeStamp"
+for i in range(len(varList)):
+    bltStr += "," + varList[i] + " REAL DEAFULT 0"
+    bltStr2 += "," + varList[i]
+bltStr += ");"
+bltStr2 += ") VALUES(?"
+for i in range(len(varList)+1):
+    bltStr2 += ",?"
+bltStr2 += ")"
+txtFileW = open("CreateTable.txt", "w+")
+txtFileW.write(bltStr)
+
+txtFileW = open("InsertTable.txt", "w+")
+txtFileW.write(bltStr2)
+
